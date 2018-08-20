e688ee92a8efd3adc28729a0705f128836bcde63
Bogdan5 <bogdan5x@gmail.com>
2018-08-19 22:12:51 -0400

    Comment

---
+++ data-searcher/src/components/DataDisplay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class DataDisplay extends Component {
+  render() {
+    return (
+      <div class={headerClass}>
+        Data searcher and organizer
+      </div>
+    );
+  }
+}
+
+export default DataDisplay;
---
+++ data-searcher/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div class={headerClass}>
+        Data searcher and organizer
+      </div>
+    );
+  }
+}
+
+export default Header;
