e500ecc465dc5f41d5957eb0981527fd5ea5dcbd
Bogdan5 <bogdan5x@gmail.com>
2018-08-20 21:37:31 -0400

    Comment

---
+++ data-searcher/src/components/Keyboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Keyboard extends Component {
+  render() {
+    return (
+      <div class={headerClass}>
+        Data searcher and organizer
+      </div>
+    );
+  }
+}
+
+export default Keyboard;
---
+++ data-searcher/src/components/Sorter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Sorter extends Component {
+  render() {
+    return (
+      <div class={sorterClass}>
+
+      </div>
+    );
+  }
+}
+
+export default Sorter;
