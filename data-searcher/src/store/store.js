870f430b21ee0452debe02c09264c5fa2347b962
Bogdan5 <bogdan5x@gmail.com>
2018-09-17 23:11:43 -0400

    Comment

--- data-searcher/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import reducer from '../reducers/reducer.js';
-
-const initialState = {};
-
-const store = createStore(reducer, initialState, window.devToolsExtension &&
-window.devToolsExtension());
-
-export default store;
