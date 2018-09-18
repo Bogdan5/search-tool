6acf78659a4d3caf15bd6302c60a5844e86db915
Bogdan5 <bogdan5x@gmail.com>
2018-09-17 23:59:45 -0400

    Comment

---
+++ data-searcher/src/components/SorterButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+// this component includes a set of buttons
+export default const SorterButton = () => {
+    return (
+        <div className='buttonGroup'>
+        {this.props.children}
+      </div>
+    )
+}
