c3e71dee65e1a70379c2ba1b06ef27fa3d69ee64
Bogdan5 <bogdan5x@gmail.com>
2018-09-23 14:50:24 -0400

    Comment

--- data-searcher/src/components/DumbButton.js
+++ data-searcher/src/components/DumbButton.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../App.css';
 
 // basic button pressed to construct queries
-class DumbButton extends Component {
-  clickHandler = () => this.props.fromButton(this.props.name);
+const DumbButton = (props) => {
+  const clickHandler = () => props.fromButton(props.name);
 
-  render() {
-    // let isActive = ['INCLUDES', 'STARTS WITH', 'ENDS WITH'].includes(this.props.name) ?
-    //   (this.props.active ? 'active' : (this.props.keywordButtonClicked === this.props.name) ?
-    //   'active' : 'inactive') : 'active';
-    
-    return (
-      <button className={`dumbButtonClass ${isActive}`}
-        onClick={this.clickHandler}>
-        {this.props.name}
-      </button>
-    );
-  }
-}
+  // let isActive = ['INCLUDES', 'STARTS WITH', 'ENDS WITH'].includes(this.props.name) ?
+  //   (this.props.active ? 'active' : (this.props.keywordButtonClicked === this.props.name) ?
+  //   'active' : 'inactive') : 'active';
+
+  return (
+    <button
+      type='submit' className={`dumbButtonClass ${isActive}`}
+      onClick={clickHandler}
+    >
+      {props.name}
+    </button>
+  );
+};
 
 export default DumbButton;
